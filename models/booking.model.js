0a7a3de378059116cec3a91af9ad9a55442a76eb
Hamza Bin Shahid <133975358+justhamzashahid@users.noreply.github.com>
2025-04-22 21:11:54 +0500

    provider side integration

--- models/booking.model.js
+++ models/booking.model.js
@@ -3,6 +3,7 @@
 const mongoose = require('mongoose');
 
 const bookingSchema = new mongoose.Schema({
+  
 
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   listing: { type: mongoose.Schema.Types.ObjectId, ref: 'Listing', required: true },
